4764bbb003e457a898b4c37374aa21536c17991a
Rodolfo Gonella <rodolfogonella@gmail.com>
2022-06-16 19:50:09 -0300

    Final

---
+++ source/app.js
@@ -0,0 +1,18 @@
+const path= require('path');
+const express= require('express');
+const app= express();
+const port= process.env.PORT || 3000;
+
+const callback = () => console.log('Server started on port ' + port);
+app.listen(port, callback);
+
+const public = path.resolve(__dirname, '../public')
+app.use(express.static(public));
+
+const home = (req, res) => res.sendFile(path.join(__dirname, '../views/home.html'))
+const login = (req, res) => res.sendFile(path.join(__dirname, '../views/login.html'))
+const register = (req, res) => res.sendFile(path.join(__dirname, '../views/register.html'))
+
+app.get('/', home);
+app.get('/', login);
+app.get('/', register);
